2e5b73ac7cd5e8d6a5cc89df625e649d73bee0aa
Camilla Barbieri <camilla.barbieri@accenture.com>
2022-06-28 12:00:36 +0200

    Notes_app

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,3 +1,12 @@
+// Aggiungo componente Sidebar sottoforma di funzione 
+// Voglio gestire diverse funzionalità: 
+// notes (default) 
+// Aggiungi notes 
+// Cancella notes 
+// Visualizzare notes attivi 
+// Scegliere con un click il note attivo
+
+
 function Sidebar ({
     notes, 
     onAddNote, 
@@ -6,17 +15,27 @@ function Sidebar ({
     setActiveNote,
 }) {
 
+// scelgo tra 2 opzioni l'ultimo che è stato modificato e lo marchio come note attivo
 const sortedNotes = notes.sort((a,b) => b.lastModified - a.lastModified );
 
 return (
      <div className="app-sidebar">
     <div className="app-sidebar-header">
         <h1>Notes</h1>
+
+{/* onAddNote è scritta sottoforma di Arrow Function in App.js in cui vengono passati dei parametri al click del button Add*/}
+
         <button onClick={onAddNote}>Add</button>
     </div>
     <div className="app-sidebar-notes">
+{/* utilizzo il metodo map e arrow function 
+utilizzo pseudo classe active per settare al click la note selezionata
+
+*/}
         {sortedNotes.map((note) => (
+
             <div 
+
             className={`app-sidebar-note ${note.id === activeNote && "active"}`} 
             onClick = {() => setActiveNote(note.id)}
             >
